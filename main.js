5567fdcbd65490f05fd609c1f92b2abef635a222
lubigsis <lucre.baluc@gmail.com>
2023-02-20 19:25:42 -0300

    Referencio algunas variables del DOM

---
+++ main.js
@@ -0,0 +1,6 @@
+//-------------------------------------Referencio variables del DOM
+
+let round = document.getElementById('round');
+let juegoBTN = document.getElementsByClassName('color');
+let empezarBTn = document.getElementById('empezarJS');
+
